6b6cd1e5880cc1b1e2ffd60c2d39955f5bdc5389
arjancupi <arjan.cupi@uefa.ch>
2019-10-30 10:56:57 +0100

    Updates

---
+++ precache-manifest.eb5520bdc0ab98fd457fd114b03ed894.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "84287d09b8053c6fa598893b8910786a",
+    "url": "/qim-quizz/static/media/logo.84287d09.svg"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/qim-quizz/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "12b6297c7e446303c27b",
+    "url": "/qim-quizz/static/js/runtime~main.12b6297c.js"
+  },
+  {
+    "revision": "86b46adaa8c2c6e638f2",
+    "url": "/qim-quizz/static/js/main.86b46ada.chunk.js"
+  },
+  {
+    "revision": "5a80405e8fff7c81f25f",
+    "url": "/qim-quizz/static/js/1.5a80405e.chunk.js"
+  },
+  {
+    "revision": "86b46adaa8c2c6e638f2",
+    "url": "/qim-quizz/static/css/main.bc10538a.chunk.css"
+  },
+  {
+    "revision": "bc54bef9dbcb988d05327ad3a04a3176",
+    "url": "/qim-quizz/index.html"
+  }
+];
