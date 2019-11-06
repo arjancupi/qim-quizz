6460002dcf75c464d8311e3ef7d08aa9f068d8c1
arjancupi <arjan.cupi@uefa.ch>
2019-11-06 15:34:35 +0100

    Updates

---
+++ precache-manifest.e0a6e1d9170b1065b849a46976e4fd4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67cbf3f8bb3688e871e4ff7eaa5944ef",
+    "url": "/qim-quizz/index.html"
+  },
+  {
+    "revision": "3b8d4d1bc244e356c2a3",
+    "url": "/qim-quizz/static/css/main.80db3eef.chunk.css"
+  },
+  {
+    "revision": "0ccd3c2a58c126b7ad5e",
+    "url": "/qim-quizz/static/js/2.61439323.chunk.js"
+  },
+  {
+    "revision": "3b8d4d1bc244e356c2a3",
+    "url": "/qim-quizz/static/js/main.89f26518.chunk.js"
+  },
+  {
+    "revision": "af43fc6b09e2566a2a78",
+    "url": "/qim-quizz/static/js/runtime-main.ce8d6f07.js"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/qim-quizz/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "ee9e8b1b8a22b14981ce838aa5f7f789",
+    "url": "/qim-quizz/static/media/logo.ee9e8b1b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/qim-quizz/precache-manifest.d572dfb24abef311c0366fe2681ad590.js"
+  "/qim-quizz/precache-manifest.e0a6e1d9170b1065b849a46976e4fd4b.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/qim-quizz/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/qim-quizz/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
